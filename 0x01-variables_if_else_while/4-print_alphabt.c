c50903ab47ab92b9e68aa97f81efdeceeaf3c045
A-levated <moyititus6@gmail.com>
2022-10-18 07:38:45 -0700

    incomplete alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabt in lower case followed by a new line, except q & e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e'; la != 'q')
+			putchar(la);
+	}
+	putchar(\n);
+
+	return (0);
+}

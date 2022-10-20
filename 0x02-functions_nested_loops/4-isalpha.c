b0018010f5776b2f9f9951f5e6aa52404f7eecd4
A-levated <moyititus6@gmail.com>
2022-10-20 00:02:30 -0700

    checks if character is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabets
+ * @c: character to be checked
+ * Return: 1 if character is a letter, 0 if not
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}

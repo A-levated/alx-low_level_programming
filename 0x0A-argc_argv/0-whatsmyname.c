390262a4aab877fe0e0a2bfbc2cab0bdbe5fa66f
A-levated <moyititus6@gmail.com>
2022-11-08 00:00:12 -0800

    prints proogramme name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main- prints file name
+ *@argc: argument count
+ *@argv: argument array
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
